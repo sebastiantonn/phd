58315e27c487260e17018a5c5613e4282afabe8d
sebastiantonn <90251517+sebastiantonn@users.noreply.github.com>
2024-05-31 23:34:21 +0200

    Update histogram-stats_singleleaf.py

--- chapter4/histogram-stats_singleleaf.py
+++ chapter4/histogram-stats_singleleaf.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  7 13:26:28 2022
 
-@author: Sebastian Tonn, based on script by Mon-Ray Shao
+"""
+@author: Sebastian Tonn and Mon-Ray Shao
 Institute: Utrecht University
-Department: Plant Microbe Interactions
+Group: Translational Plant Biology
 Lab: Guido van den Ackerveken
 """
 
 ### This script takes images made with the TUD imager, corrects brightness with reference image,
-### sets everything on the right of a defined x coordinate to black/0, segments the leaves, and generates histogram statistics.
-### It is meant for images of single leaves taken using the "Otto-plant-holder",
-### where we only want to get information from the one isolated leaf, but some parts of the rest of the plant are still
-### in the image on the right hand side and have to be "cut" out.
-### 
+### segments the leaves or plants, and generates histogram statistics.
+### It is meant for images of single leaves or plants as all segmented objects will be analyzed together.
+### The histogram statistics are saved in two CSV files (one for UV images, one for white light images).
+### Additionally, for visualization the script saves the segmented leaf images (both UV and WL) for visualization.
+### There are several optional arguments to adjust the output, see descriptions below.
 
 ### Load packages
 from PIL import Image
